9a8aecc1fc7c77ac563d5cd0991d96178a962c02
JanDabrowski <r.j.dabrowski@outlook.com>
2023-04-24 12:01:08 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,26 @@
 komenda=$1
 
-if [ $komenda = "--date" ]
+if [ $komenda = "--logs" ]
 then
-	echo $(date)
+	arg="$#"
+	if [ $arg = "2" ]
+	then
+		ARGUMENT=$2
+		d=1
+		for ((i=$d; i<=$ARGUMENT; i++))
+		do
+			touch log$i.txt
+			echo "log"$i > log$i.txt
+			echo  $(date) >> log$i.txt
+		done
+	else
+                for i in {1..100}
+                do
+                        touch log$i.txt
+                        echo "log"$i > log$i.txt
+                        echo  $(date) >> log$i.txt
+                done
+	fi
+	
 
 fi
