e241bae8a8d32165a172ebd8574e0a6b61a908a5
JanDabrowski <r.j.dabrowski@outlook.com>
2023-04-24 12:05:47 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,8 @@
 komenda=$1
 
-if [ $komenda = "--date" ]
+if [ $komenda = "--help" ]
 then
-	echo $(date)
-
+	echo "--date - Wyswietla aktualna date"
+	echo "--logs (ilosc logow) - tworzy pliki z logami (domyslnie 100)"
+	echo "--help - wyswietla wlasnie te wiadomosc"
 fi
